b5db5a8baf5380ee792cdac318fc0d14ace048e3
Fabio Fernandes <boicotinho@gmail.com>
2022-02-04 23:08:21 +0200

    updated test_install and bitfinex_cpp external

--- tests/test_install.sh
+++ tests/test_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+ROOT_DIR=$(dirname ${SCRIPT_DIR})
+
 source /opt/rh/rh-python36/enable
-pip install ../
+pip install ${ROOT_DIR}
 echo "===== Running Test ====="
 python -c "import bitfinex_quotes as m; print('RESULT m.add(1, 2) = ' + str(m.add(1, 2)))"
 echo "===== Test result: $?"
 pip uninstall -y bitfinex_quotes
-
