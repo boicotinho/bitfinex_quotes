ea60fcbbc13ae51d61433fe95ffa6a86d10d4f50
Fabio Fernandes <boicotinho@gmail.com>
2022-02-14 16:12:03 +0200

    bump bitfinex_cpp

--- docker/run_docker_image_interactive.sh
+++ docker/run_docker_image_interactive.sh
@@ -1,4 +1,4 @@
 #sudo docker run -it --mount type=bind,source=/home/fabio/docker_projects/home_mnt,target=/root/share/ bitfinex_dock -cap-add=SYS_PTRACE --security-opt seccomp=unconfined bitfinex_quotes
 #sudo docker run -it bitfinex_dock
 # Allow gdb to attach with ASLR (address space layout randomization - security feature)
-sudo docker run -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  bitfinex_dock bitfinex_quotes
+sudo docker run -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  bitfinex_dock
